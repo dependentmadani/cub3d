0dd98b183ad10beb12e2649a7f5770e483430691
dependentmadani <madani.badaoui12@gmail.com>
2022-09-12 14:10:11 +0100

    it starts raycasting, need to fix the segfault

--- raycasting/checker_v1.c
+++ raycasting/checker_v1.c
@@ -25,6 +25,7 @@ int draw_line(t_game *game, double end_x, double end_y, int color, int angle)
 
     d_x = end_x - game->gamer->player_posx;
     d_y = end_y - game->gamer->player_posy;
+    // printf("the value of d_x {%f} and d_y {%f}\n", d_x,d_y);
     pixel = sqrt((d_x * d_x) + (d_y * d_y));
     d_x /= pixel;
     d_y /= pixel;
@@ -32,9 +33,19 @@ int draw_line(t_game *game, double end_x, double end_y, int color, int angle)
     pixel_dy = game->gamer->player_posy;
     while (pixel)
     {
+        if (pixel_dx > game->mapp->map_x*IMG_H || pixel_dx < 0)
+        {
+            pixel--;
+            continue;
+        }
+        if (pixel_dy > game->mapp->map_y*IMG_W || pixel_dy < 0)
+        {
+            pixel--;
+            continue;
+        }
         mlx_pixel_put(game->mlx, game->win, pixel_dx, pixel_dy, color);
         pixel_dx += d_x;
-        pixel_dy += d_y;        
+        pixel_dy += d_y;
         pixel--;
     }
     return (0);
@@ -43,18 +54,18 @@ int draw_line(t_game *game, double end_x, double end_y, int color, int angle)
 double dist(double ax, double ay, double bx, double by, double ang)
 {
     (void)ang;
-    return (sqrt((bx - ax)* (bx - ax) + (by - ay)* (by-ay)));
+    return (sqrt((bx - ax)*(bx - ax) + (by - ay)* (by-ay)));
 }
 
 void    spread_rays(t_game *game)
 {
     int r, fov;
-    double rx, ry, ra,xo, yo, disH, hx, hy, aTan;
+    double rx, ry, ra,xo, yo, disH, hx, hy, aTan, nTan;
 
-    ra = game->gamer->player_angle-DR*30; if (ra<0) {ra +=2*PI;} if (ra > 2*PI) {ra-=2*PI;};
+    // ra = game->gamer->player_angle-DR*30; if (ra<0) {ra +=2*PI;} if (ra > 2*PI) {ra-=2*PI;};
     // printf("the value of angle {%f}\n", ra);
-    // ra = game->gamer->player_angle;
-    for (r=0; r< 60; r++)
+    ra = game->gamer->player_angle;
+    for (r=0; r< 1; r++)
     {
         ///// check the horizontal lines
         fov = 0;
@@ -63,37 +74,41 @@ void    spread_rays(t_game *game)
         if (ra < PI) {ry =(((int)game->gamer->player_posy/64)*64)-0.0001; rx=(game->gamer->player_posy-ry)*aTan+game->gamer->player_posx; yo=-64; xo=-yo*aTan;}
         if (ra > PI) {ry =(((int)game->gamer->player_posy/64)*64)+64; rx=(game->gamer->player_posy-ry)*aTan+game->gamer->player_posx; yo=64; xo=-yo*aTan;}
         if (ra == 0 || ra==PI) {rx=game->gamer->player_posx; ry=game->gamer->player_posy;fov=8;}
+        // printf("before: \tpos x is {%f}\tthe value of rx {%f} and the value of ry {%f} angle nTan is {%f}\n", game->gamer->player_posx, rx, ry, aTan);
         while (fov < 8)
         {
             if (collision_with_wall(game, rx, ry)){hx=rx; hy=ry; disH=dist(game->gamer->player_posx, game->gamer->player_posy, hx, hy, ra); fov=8;}
             else {rx+=xo; ry+=yo;fov+=1;}
         }
+        // printf("first: \tpos x is {%f}\tthe value of hx {%f} and the value of hy {%f} angle nTan is {%f}\n", game->gamer->player_posx, hx, hy, aTan);
         // printf("the value of hx {%f} the value of hy {%f}\n", hx, hy);
-        
-        
-        
+        // draw_line(game, rx, ry,0xffff00, ra);
         
         // /// check the vertical lines
         
-        // fov = 0;
-        // double disV=1000000, vx=game->gamer->player_posx, vy=game->gamer->player_posy;
-        // double nTan = -1*tan(ra);
-        // if (ra > P2 && ra < P3) {rx =(((int)game->gamer->player_posx/64)*64)-0.0001; ry=(game->gamer->player_posx-rx)*nTan+game->gamer->player_posy; xo=-64; yo=-xo*nTan;}
-        // if (ra < P2 || ra > P3) {rx =(((int)game->gamer->player_posx/64)*64)+64; ry=(game->gamer->player_posx-rx)*nTan+game->gamer->player_posy; xo=64; yo=-xo*nTan;} // problem need to be fixed here
-        // if (ra == 0 || ra==PI) {rx=game->gamer->player_posx; ry=game->gamer->player_posy;fov=8;}
+        fov = 0;
+        double disV=1000000, vx=game->gamer->player_posx, vy=game->gamer->player_posy;
+        nTan = -1*tan(ra);
+        if (ra > P2 && ra < P3) {rx =(((int)game->gamer->player_posx/64)*64)+64; ry=(game->gamer->player_posx-rx)*nTan+game->gamer->player_posy; xo=64; yo=-xo*nTan;}
+        if (ra < P2 || ra > P3) {rx =(((int)game->gamer->player_posx/64)*64)-0.0001; ry=(game->gamer->player_posx-rx)*nTan+game->gamer->player_posy; xo=-64; yo=-xo*nTan;} // problem need to be fixed here
+        if (ra == 0 || ra==PI) {rx=game->gamer->player_posx; ry=game->gamer->player_posy;fov=8;}
         // printf("before, the value of xo {%f} and y0 {%f}\n", xo, yo);
-        // while (fov < 8)
-        // {
-        //     if (collision_with_wall(game, rx, ry)){vx=rx; vy=ry; disV=dist(game->gamer->player_posx, game->gamer->player_posy, vx, vy, ra); fov=8;}
-        //     else {rx+=xo; ry+=yo;fov+=1;}
-        // }
-        // printf("the value of vx {%f} the value of vy {%f}\n", vx, vy);
-        // if (disV < disH) {rx = vx; ry =vy;}
-        // if (disH < disV) {rx = hx; ry =hy;}
-
+        // printf("first: \tpos x is {%f}\tthe value of rx {%f} and the value of ry {%f} angle nTan is {%f}\n", game->gamer->player_posx, rx, ry, nTan);
+        // printf("position of player in x {%f} and in y {%f}\n", game->gamer->player_posx, game->gamer->player_posy);
+        while (fov < 8)
+        {
+            if (collision_with_wall(game, rx, ry)){vx=rx; vy=ry; disV=dist(game->gamer->player_posx, game->gamer->player_posy, vx, vy, ra); fov=8;}
+            else {rx+=xo; ry+=yo;fov+=1;}
+        }
+        // printf("second: \tpos x is {%f}\tthe value of vx {%f} and the value of vy {%f} angle nTan is {%f}\n", game->gamer->player_posx, vx, vy, nTan);
+        // printf("the value of disV {%f} the value of disH {%f}\n", disV, disH);
+        if (disV < disH) {rx = vx; ry =vy;}
+        if (disH < disV) {rx = hx; ry =hy;}
         draw_line(game, rx, ry,0xff0000, ra);
+        // printf("what it tooks is: rx {%f} and ry {%f} and disH = {%f} and disV = {%f}\n", rx, ry, disH, disV);
         ra += DR;
         if (ra<0) {ra +=2*PI;} if (ra > 2*PI) {ra-=2*PI;};
+        // printf("\n");
     }
-    printf("all good and the value of the angle is {%f}\n", ra);
+    // printf("all good and the value of the angle is {%f}\n", ra);
 }
